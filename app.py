909991b85ce14e73d398796d2f8f4ad881011ab8
Ethan <theethancastro@gmail.com>
2023-10-02 08:28:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+import streamlit as st
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
